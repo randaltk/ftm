e7aa12db851e92a591546955a33038cfdc4dc573
randaltk <randal.t.krebs@gmail.com>
2021-11-06 23:57:23 -0300

    Ids locacao

--- pages/locacao/[id].js
+++ pages/locacao/[id].js
@@ -1,55 +1,27 @@
 import { locacaoMock } from "./mock";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "../../styles/Home.module.scss";
 import Link from "next/link";
+import { withRouter } from "next/router";
 import Image from "next/image";
-export default function Details() {
-  function detailLocacao() {
-    const paths = locacaoMock.map((locacao) => {
-      return {
-        params: {
-          id: locacao.id,
-        },
-      };
-    });
-  }
+
+function Details(props) {
   return (
     <section className={styles.sessaoSobre}>
       <div className={styles.contentPresentation}>
         <div className={styles.sessaoContent}>
-          {/* <div className={styles.borderStyle}></div>
-          <h2 className={styles.title}> História</h2>
-          <div className={styles.contentBanner}>
-            <Image
-              src="https://res.cloudinary.com/aguadeira/image/upload/v1624840207/FTM/pexels-kervin-edward-lara-3976320_kmwwlz.jpg"
-              alt="Inspeção"
-              width={438.08}
-              height={350.38}
-            />
-          </div>
-
-          <p className={styles.description}>
-            A FTM foi fundada no Brasil em 2002, trazendo know how, e inovações
-            tecnológicas ao mercado brasileiro. Tem como foco principal a
-            qualidade e competitividade no mercado, através de seu atendimento
-            personalizado desde a comercialização de seus produtos e serviços,
-            até o pós venda. Nossa Missão é, satisfazer nossos clientes,
-            oferecendo conforto, praticidade e segurança dos produtos e serviços
-            ofertados. Recompensados pelo nosso trabalho, nos tornamos, uma das
-            principais empresas do ramo de venda, locação de chaves de torque e
-            tensionadores hidráulicos, além de projetos especiais e apoio
-            técnico no seguimento de torqueamento e tensionamento controlado de
-            parafusos, onde atendemos vários setores da nossa economia do
-            mercado brasileiro. Nosso maior patrimônio é a satisfação de nossos
-            clientes, além da preocupação com a segurança dos usuários dos
-            nossos produtos.
-          </p> */}
           <section className={styles.sessaoLocacao}>
             <div>
               <h2>Locação</h2>
             </div>
+
             <li className={styles.navItem}>
               <Link href="/">Home</Link> <span>{"»"}</span>
               <Link href="/locacao">Locação</Link>
+              <span>{"»"}</span>
+              <Link href={`locacao/${props.router.query.id}`}>
+                <span>{props.router.query.nome}</span>
+              </Link>
             </li>
           </section>
         </div>
@@ -57,3 +29,4 @@ export default function Details() {
     </section>
   );
 }
+export default withRouter(Details);
--- pages/locacao/index.js
+++ pages/locacao/index.js
@@ -6,11 +6,13 @@ import { useRouter } from "next/router";
 import { locacaoMock } from "./mock";
 export default function Locacao() {
   const router = useRouter();
-  const routeChange = (e, id) => {
-    let path = `locacao/${id}`;
-
+  const routeChange = (e, nome, id) => {
     e.preventDefault();
-    router.push(path);
+    return router.push({
+      pathname: `locacao/${id}`,
+
+      query: { nome: nome },
+    });
   };
 
   return (
@@ -26,10 +28,11 @@ export default function Locacao() {
         </div>
 
         <div className={styles.cardsLocacao}>
-          {locacaoMock.map((locacao) => (
+          {locacaoMock.map((locacao, key) => (
             <div
               className={styles.cardsContent}
-              onClick={(e) => routeChange(e, locacao.id)}
+              onClick={(e) => routeChange(e, locacao.nome, locacao.id)}
+              key={key}
             >
               <Image
                 className={styles.cardsContentImg}
@@ -43,97 +46,6 @@ export default function Locacao() {
             </div>
           ))}
         </div>
-        {/* <div className={styles.cardsLocacao}>
-          <div
-            className={styles.cardsContent}
-            onClick={(e) => routeChange(e, "locacao")}
-          >
-            <Image
-              className={styles.cardsContentImg}
-              src="https://res.cloudinary.com/aguadeira/image/upload/v1624832143/FTM/f-10-serie-f_1_emdwpg.jpg"
-              alt="Caldeiras"
-              width={500}
-              height={500}
-            />
-            <div className={styles.cardsDescription}>
-              <h3>Locação de Máquinas e Ferramentas</h3>
-              <p>
-                Por que alugar?
-                <ul className={styles.detail}>
-                  <li className={styles.topic}>
-                    Disponibilidade imediata dos Equipamentos.
-                  </li>
-                  <li className={styles.topic}>Facilidade de Gerenciamento</li>
-                  <li className={styles.topic}>Benefício Fiscal</li>
-                  <li className={styles.topic}>Facilidade de Contratação</li>
-                </ul>
-              </p>
-            </div>
-          </div>
-
-          <div
-            className={styles.cardsContent}
-            onClick={(e) => routeChange(e, "venda")}
-          >
-            <Image
-              className={styles.cardsContentImg}
-              src="https://res.cloudinary.com/aguadeira/image/upload/v1624832521/FTM/fch-10-serie-champion_2_rgf5w3.jpg"
-              alt="Válvulas"
-              width={500}
-              height={500}
-            />
-            <div className={styles.cardsDescription}>
-              <h3>Venda de Máquinas e Ferramentas</h3>
-              <p>
-                Oferecemos equipamentos genuinamente nacional, com tecnologia
-                comparado aos equipamentos importados. Equipamentos Calibrados
-                RBC com garantias superiores comparado ao que o mercado oferece.
-              </p>
-            </div>
-          </div>
-
-          <div
-            className={styles.cardsContent}
-            onClick={(e) => routeChange(e, "assistencia")}
-          >
-            <Image
-              className={styles.cardsContentImg}
-              src="https://res.cloudinary.com/aguadeira/image/upload/v1624833289/FTM/fch-1-serie-champion_1_dbj87n.jpg"
-              alt="Escadas"
-              width={500}
-              height={500}
-            />
-            <div className={styles.cardsDescription}>
-              <h3>Assistência Técnica</h3>
-              <p>
-                A FTM conta com uma equipe técnica altamente qualificada para
-                manutenção de máquinas e ferramentas do segmento hidráulico,
-                mecânico, elétrico, pneumático e manual.
-              </p>
-            </div>
-          </div>
-
-          <div
-            className={styles.cardsContent}
-            onClick={(e) => routeChange(e, "assistencia")}
-          >
-            <Image
-              className={styles.cardsContentImg}
-              src="https://res.cloudinary.com/aguadeira/image/upload/v1624833289/FTM/fch-1-serie-champion_1_dbj87n.jpg"
-              alt="Escadas"
-              width={500}
-              height={500}
-            />
-            <div className={styles.cardsDescription}>
-              <h3>Assistência Técnica</h3>
-              <p>
-                A FTM conta com uma equipe técnica altamente qualificada para
-                manutenção de máquinas e ferramentas do segmento hidráulico,
-                mecânico, elétrico, pneumático e manual.
-              </p>
-            </div>
-          </div>
-        </div> */}
       </section>
     </>
   );
