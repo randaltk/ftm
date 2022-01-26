3d3ad77306e9e6706adca4638ae08b06e4b3bb86
Randal <randal.t.krebs@gmail.com>
2022-01-26 13:30:33 -0300

    att items

--- mocks/locacaoMock.js
+++ mocks/locacaoMock.js
@@ -54,84 +54,83 @@ const locacaoMock = [
       "https://res.cloudinary.com/aguadeira/image/upload/v1642441075/FTM/vendas/fcharq_ylncw3.jpg",
   },
   {
-    id: 2,
-    nome: "FCH",
+    id: 3,
+    nome: "Multiplicador de Torque",
     imagem:
-      "https://res.cloudinary.com/aguadeira/image/upload/v1642440765/FTM/vendas/fch_pdkaxz.jpg",
-    descricao: `Ferramenta utilizada quando se requer o torque controlado dos parafusos, muito utilizado para o aperto de flanges, trocadores de calor, gruas, montagem de máquinas industriais. 
-    Chave de torque de alto desempenho oferece um design compacto, construído em liga de alumínio-titânio alta resistência para maior intensidade e durabilidade da ferramenta, minimizando o peso. 
-    Swivel podendo ser ajustado em 360° e 180° grau facilitando a conexão da mangueira permitindo ajuste fácil em espaços confinados, eliminando as dobras.
-    `,
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643071441/FTM/vendas/ftmumiljha_1_r9ftdo.png",
+    descricao: `Equipamento utilizado nos serviços de aparafusamento com grande eficiência e design compacto, alcançando torque alto na saída a partir de torque baixo na entrada.
+    Multiplicador estilo LINK com o melhor para a indústria em relação a característica Resistencia-Peso com trava anti-retorno para proteção do operador.
+    Os Multiplicadores FTM tem a capacidade de multiplicar o torque de entrada em 4:1 para o torque de saída. `,
     fichaTecnica: {
       pressao: "Design Compacto",
       saidas: "Melhor na indústria relação resistência-peso ",
-      acionamento: "Pressão Máxima 700 bar",
-      motor: "•	Faixa de torque 272 à 72.000 N.m ",
-      tensao: "Certificado de calibração rastreado RBC ",
-      peso: "Drive quadrado: de 1/2” à 2 ½” que abrange uma grande quantidade de soquetes.",
+      acionamento: "Entrada Máxima de 340 N.m à Saída máxima de 1.360 N.m",
+      motor: "Fator de Multiplicação 4:1 ",
+      tensao: "Certificado de calibração rastreado RBC",
+      peso: "Entrada (Quadrado) ½” / Saída (Sextavado) 46 mm",
       dimensoes:
-        "Swivel podendo ser ajustado em 360° e 180° grau facilitando a conexão da mangueira permitindo ajuste fácil em espaços confinados, eliminando as dobras. ",
-      vazao: "1,02 l/min",
-      capacidade:
-        "Trava anti-reverso, projetada para impedir o retorno da catraca para manter o torque necessário. ",
+        "Trava anti-reverso, projetada para impedir o retorno da catraca para manter o torque necessário",
     },
     imagemTabela:
-      "https://res.cloudinary.com/aguadeira/image/upload/v1642441202/FTM/vendas/fcharq_t4rm9y.png",
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643131392/FTM/vendas/ftm2lte22_pvd0lo.png",
     imagemArq:
-      "https://res.cloudinary.com/aguadeira/image/upload/v1642441075/FTM/vendas/fcharq_ylncw3.jpg",
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643125866/FTM/vendas/ftm2lalala_qtfwmz.png",
   },
   {
-    id: 3,
-    nome: "Multiplicador de Torque ",
+    id: 4,
+    nome: "FLCT",
     imagem:
-      "https://res.cloudinary.com/aguadeira/image/upload/v1636239540/FTM/Mangueiras-Hidraulica-Duplas1_el54b5.jpg",
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643132201/FTM/vendas/Sem_t%C3%ADtulo_elt6rg.png",
 
-    descricao: `Equipamento utilizado nos serviços de aparafusamento com grande eficiência e design compacto, alcançando torque alto na saída a partir de torque baixo na entrada. 
-    Multiplicador estilo LINK com o melhor para a indústria em relação a característica Resistencia-Peso com trava anti-retorno para proteção do operador. 
-    Os Multiplicadores FTM tem a capacidade de multiplicar o torque de entrada em 4:1 para o torque de saída. 
+    descricao: `Assim como a linha F a linha FLCT também é uma chave de torque tipo LINK e que tem características bem parecidas nos tipos de serviços como áreas de difícil acesso e pouco espaço, também muito utilizada em plataformas de petróleo, usinas de cana de açúcar, hidrelétricas e gasodutos.
+    O Link é versátil suporta uma vasta gama de sextavados com cassetes intercambiáveis.
     `,
     fichaTecnica: {
-      pressao: "700 bar 10.000 PSI",
-      saidas: "4",
-      acionamento: "Elétrico",
-      motor: "Elétrico: 1 1/8 HP 12.000 RPM",
-      tensao: "220V",
-      peso: "36 kg",
-      dimensoes: "AxLxC = 460x353x333 mm",
-      vazao: "1,02 l/min",
-      capacidade: "Reservatório: 9,46 litros",
+      pressao: "Design Compacto",
+      saidas:
+        "Cabeçote feito de liga Alumínio-Titânio melhorando no peso do equipamento e com alta resistência.",
+      acionamento: "Pressão Máxima 700 bar",
+      motor: "Faixa de torque 260 à 43.100 N.m",
+      tensao: "Certificado de calibração rastreado RBC",
+      peso: "Link propulsor Hidráulico de alto desempenho oferece um design compacto, com cabeçote construído em liga de alumínio-titânio alta resistência para maior intensidade e durabilidade da ferramenta, minimizando o peso.",
+      dimensoes:
+        "Swivel podendo ser ajustado em 360° e 180° grau facilitando a conexão da mangueira permitindo ajuste fácil em espaços confinados, eliminando as dobras.",
+      vazao:
+        "Swivel podendo ser ajustado em 360° e 180° grau facilitando a conexão da mangueira permitindo ajuste fácil em espaços confinados, eliminando as dobras.",
+      capacidade: "",
     },
     imagemTabela:
-      "https://res.cloudinary.com/aguadeira/image/upload/v1636290344/FTM/tabela-ftm_tyo3jk.jpg",
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643132688/FTM/vendas/Sem_t%C3%ADtulo_2_b7zn0c.png",
+    imagemArq:
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643132546/FTM/vendas/Sem_t%C3%ADtulo_1_jpdnuk.png",
   },
-  // {
-  //   descricao: `Bomba Hidráulica com acionamento elétrico para uso em até quatro chaves de torque e Link Hidráulico simultâneos para serviço de torqueamento.
-  //   Com manômetro calibrado rastreado RBC`,
-  //   imagemTabela: "",
-  //   nome: "Link Propulsor",
-  //   image:
-  //     "https://res.cloudinary.com/aguadeira/image/upload/v1636239540/FTM/Link-Propulsor_pixckc.jpg",
-  //   id: 2,
-  // },
-  // {
-  //   descricao: "",
-  //   imagemTabela: "",
-  //   nome: "Chave de torque",
-  //   image:
-  //     "https://res.cloudinary.com/aguadeira/image/upload/v1636239540/FTM/Chave-de-Torque_tfexzl.jpg",
 
-  //   id: 3,
-  //   descricao: "",
-  //   imagemTabela: "",
-  // },
-  // {
-  //   nome: "Mangueiras Hidráulicas Duplas",
-  //   image:
-  //     "https://res.cloudinary.com/aguadeira/image/upload/v1636239540/FTM/Mangueiras-Hidraulica-Duplas1_el54b5.jpg",
+  {
+    id: 4,
+    nome: "Tensionador WIND",
+    imagem:
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643213933/FTM/vendas/Sem_t%C3%ADtulo_3_kgopva.png",
 
-  //   id: 4,
-  //   descricao: "",
-  //   imagemTabela: "",
-  // },
+    descricao: `Os Tensionadores FTM da linha WIND foram projetados para atender a indústria de geração de energia eólica, vapor e gás, principalmente na indústria eólica.
+    Os Tensionadores WIND tem alto desempenho, alta resistência, durabilidade e facilidade no uso do equipamento.
+    Nossos Tensionadores são calibrados em laboratório com rastreabilidade RBC.
+    `,
+    fichaTecnica: {
+      pressao: "Vedações com grande durabilidade e alta resistência.",
+      saidas: "Pressão máxima de 1350 bar",
+      acionamento: "Faixa de força de 134,4 à 2.213 k.N",
+      motor: "Certificado de calibração com rastreabilidade RBC",
+      tensao:
+        "Indicador de fim de curso: Indica para que o curso máximo não seja ultrapassado",
+      peso: "Conexão de engate rápido, mais segura e simples",
+      dimensoes: "",
+      vazao: "",
+      capacidade: "",
+    },
+    imagemTabela:
+      "https://res.cloudinary.com/aguadeira/image/upload/v1643214100/FTM/vendas/Sem_t%C3%ADtulo_4_pzncz8.png",
+    imagemArq: "/",
+  },
+  ,
 ];
 export default locacaoMock;
