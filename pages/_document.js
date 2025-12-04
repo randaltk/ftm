1bf7ed30c29f02654e1752a4843562bfcbdd039c
Randal <randal.t.krebs@gmail.com>
2025-12-04 11:14:02 -0300

    add google analytics , tag manager, ads

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,39 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WTXKGFQV');
+              `,
+            }}
+          />
+
+          {/* Google tag (gtag.js) - shared for Google Analytics 4 and Google Ads */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-WEQJXFYBE0"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                // Google Analytics 4
+                gtag('config', 'G-WEQJXFYBE0');
+                // Google Ads
+                gtag('config', 'AW-16679702051');
+              `,
+            }}
+          />
+
           <meta
             name="description"
             content="EQUIPAMENTOS HIDRÁULICOS. Alicate Prensa Terminais. Diversos Modelos e Capacidades. Bomba Hidráulica p/ Torqueamento. Acionamento: ELÉTRICO ..."
@@ -23,6 +56,15 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTXKGFQV"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
